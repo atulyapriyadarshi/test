9fcdd416771e971a3b4dc31a455dae76db50016f
Atulya <atulya.priyadarshi@verts.co.in>
2014-03-06 12:59:55 +0530

    The removed

--- fleet/__openerp__.py
+++ fleet/__openerp__.py
@@ -19,7 +19,7 @@
 #
 ##############################################################################
 {
-    'name' : 'the Fleet Management',
+    'name' : 'Fleet Management',
     'version' : '0.1',
     'author' : 'OpenERP S.A.',
     'category': 'Managing vehicles and contracts just i am checkinggggggg',
